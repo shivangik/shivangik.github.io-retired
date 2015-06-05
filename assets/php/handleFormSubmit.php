ef454e1753f79d3ebb9c65b28ef552b33610de39
shivangik <khanna.shivangi@gmail.com>
2015-06-04 21:09:24 -0400

    With Contact and Subjects

--- assets/php/handleFormSubmit.php
+++ assets/php/handleFormSubmit.php
@@ -1,10 +1,24 @@
 <?php
 
 if(isset($_POST['c_name'])){
-    
-    $res['sendstatus'] = 1;
-    $res['message'] = 'Form Submission Succesful';
-    echo json_encode($res);
+
+  $to = "khanna.shivangi@gmail.com";
+  $from = $_REQUEST['c_email'];
+  $name = $_REQUEST['c_name'];
+  $headers = "From: $from";
+  $subject = "[ACTION REQUIRED] Tutoring Request";
+
+  $fields = array();
+  $fields{"name"} = "c_name";
+  $fields{"email"} = "c_email";
+  $fields{"message"} = "c_message";
+
+  $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
+  $send = mail($to, $subject, $body, $headers);
+  $res['sendstatus'] = 1;
+  $res['message'] = 'Form Submission Succesful';
+  echo json_encode($res);
 }
 
 ?>
